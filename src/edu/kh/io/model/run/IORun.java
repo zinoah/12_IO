4eb652cff035e41458085a1fc9e15c3cbc41a509
Jinho <zinoah@gmail.com>
2022-12-30 11:39:32 +0900

    IO commit

---
+++ src/edu/kh/io/model/run/IORun.java
@@ -0,0 +1,17 @@
+package edu.kh.io.model.run;
+
+import edu.kh.io.model.service.IOService;
+
+public class IORun {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		IOService ios = new IOService();
+		
+		//ios.output1();
+		//ios.input1();
+		ios.output2();
+		//ios.input2();
+	}
+
+}
